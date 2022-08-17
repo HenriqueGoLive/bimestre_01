41205e4bed6dfa0b93c4eeb2b3566b1200e8e2f7
HenriqueGoLive <henrique.gabriel.de.oliveira43@gmail.com>
2022-08-16 21:17:36 -0300

    atividade_01

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Henrique Gabriel de Oliveira
         </a>
       </header>
     </div>
